98fdd78f7aa68886fc335e2a56f8a182f9fe8c73
yagofalcao <42628080+yagofalcao@users.noreply.github.com>
2019-09-06 18:57:28 -0300

    Update quiz_2.py

--- quiz_2.py
+++ quiz_2.py
@@ -1,20 +1,19 @@
-def valor(n):
-    notas = [100.00, 50.00, 20.00, 10.00, 5.00, 1.00, 0.50, 0.25, 0.10, 0.5, 0.01]
-    notas.sort()
-    notas.reverse()
-    numNotas = []
-    for i in notas:
-        if n/i == 0:
-            numNotas.append(n/i)
-        if n/i < 0:
-            pass
-        if n/i > 0:
-            numNotas.append(n/i)
-            n %= i
+#FINAL - CONTANDO CEDULAS
+
+def numero(x):
+
+    notas = [100.00, 50.00, 20.00, 10.00, 5.00, 2.00, 1.00, 0.50, 0.25, 0.10, 0.5, 0.01]
+    #Coloquei para ler cada nota
     for i in range(len(notas)):
-        print (notas[i], numNotas[i])
+        #salvei em a o valor do numero divido pela nota
+        a = x/notas[i]    
+        if int(a)>0:
+            print (int(a), 'nota(s) de', notas[i])
+        x -= notas[i]*int(a)  
+        i += 1
         
-valor(150)
+numero(275.50)
+
 
 """
 A classe bola representa uma esfera cheia de agua (com peso 1000g por metro cubico) com um raio r 
@@ -25,8 +24,38 @@ Obs: volume da espera = (4/3)*pi*(r**3), area da superfice = 4*pi*(r**2)
 Ex: bola((2, "vermelho")).peso -> 33.66012533333334
     bola((3, "azul")).peso     -> 113.20709400000001
 """
+
+#BOLA
+
 class bola:
-    def __init__(self, tamanho,cor):
-        self.tamanho = tamanho
-        self.cor = "azul" or "amarelo" or "vermelho"
-pi = 3.14159
+   
+    def __init__(self,r,cor):
+        self.r = r
+        self.cor = cor
+        
+    def peso(self):
+        pi = 3.14159
+        
+        if self.cor == "azul" or "Azul":
+            x = 1
+        if self.cor == "amarelo" or "Amarelo":
+            x = 2
+        if self.cor == "vermelho" or "Vermelho":
+            x = 3
+        else:
+            print("Você deve escolher as cores: azul, amarelo ou vermelho")
+            
+        #V = volume da esfera
+        V = (4/3)*pi*(self.r**3)
+        
+        #kg por m³
+        
+        #AS = Área da Superfície
+        AS = 4*pi*(self.r**2)
+        
+        peso = V + AS*x
+        
+        return peso
+
+bola_x = bola(5,"vermelho")
+bola_x.peso()
